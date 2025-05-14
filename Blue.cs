77a20e702aaed46dc327e3495cf0179fd92be187
mrkvrn <m2402828@edu.misis.ru>
2025-05-14 19:46:40 +0300

    Add files via upload

---
+++ Blue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public abstract class Blue
+    {
+        protected string _input;
+
+        public string Input => _input;
+
+        public Blue(string input)
+        {
+            _input = input;
+        }
+
+        public abstract void Review();
+    }
+}
